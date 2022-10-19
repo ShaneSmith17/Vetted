9e5f18b601518fc46094c0c79623a61ba9d18f9f
ShaneSmith17 <hello.shane.smith@gmail.com>
2022-10-19 15:29:57 -0400

    Submissions components

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import SubmissionsIndexContainer from "./SubmissionsIndexContainer"
+
 export const App = (props) => {
   return (
     <Router>
       <Switch>
+        <Route to="/" component={SubmissionsIndexContainer} />
+        <Route to="/submissions" />
       </Switch> 
     </Router>
   )
 }
 
-export default App
+export default App;
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,4 @@
-class Submissions < ApplicationRecord 
+class Submission < ApplicationRecord 
   validates :title, presence: true 
   validates :body, presence: true 
 end 
